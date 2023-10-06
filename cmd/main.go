072f145a784dc73bc883f378e86b3f792188c863
Luther Monson <lmonson@akamai.com>
2023-10-06 11:22:06 -0700

    removing the internal directory

--- cmd/main.go
+++ cmd/main.go
@@ -18,6 +18,7 @@ package main
 
 import (
 	"flag"
+	controller2 "github.com/linode/cluster-api-provider-linode/controller"
 	"os"
 
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
@@ -33,7 +34,6 @@ import (
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 
 	infrastructurev1alpha1 "github.com/linode/cluster-api-provider-linode/api/v1alpha1"
-	"github.com/linode/cluster-api-provider-linode/internal/controller"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -89,14 +89,14 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err = (&controller.LinodeClusterReconciler{
+	if err = (&controller2.LinodeClusterReconciler{
 		Client: mgr.GetClient(),
 		Scheme: mgr.GetScheme(),
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "LinodeCluster")
 		os.Exit(1)
 	}
-	if err = (&controller.LinodeMachineReconciler{
+	if err = (&controller2.LinodeMachineReconciler{
 		Client: mgr.GetClient(),
 		Scheme: mgr.GetScheme(),
 	}).SetupWithManager(mgr); err != nil {
