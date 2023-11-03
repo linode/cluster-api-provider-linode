dd786d4bf4a158c821ea711f414a80b236e44fa1
Zhiwei Liang <zhi.wei.liang@outlook.com>
2023-11-03 04:14:34 -0400

    Add cluster scope

---
+++ cloud/scope/cluster.go
@@ -0,0 +1,51 @@
+/*
+Copyright 2023 Akamai Technologies, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package scope
+
+import (
+	"context"
+
+	infrav1 "github.com/linode/cluster-api-provider-linode/api/v1alpha1"
+	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
+	"sigs.k8s.io/cluster-api/util/patch"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// ClusterScopeParams defines the input parameters used to create a new Scope.
+type ClusterScopeParams struct {
+	LinodeClients
+	Client     client.Client
+	Cluster    *clusterv1.Cluster
+	LinodeCluster *infrav1.LinodeCluster
+}
+
+// NewClusterScope creates a new Scope from the supplied parameters.
+// This is meant to be called for each reconcile iteration.
+func NewClusterScope(ctx context.Context, params ClusterScopeParams) (*ClusterScope, error) {
+	// TODO
+	return &ClusterScope{}, nil
+}
+
+// ClusterScope defines the basic context for an actuator to operate upon.
+type ClusterScope struct {
+	client      client.Client
+	patchHelper *patch.Helper
+
+	LinodeClients
+	Cluster    *clusterv1.Cluster
+	GCPCluster *infrav1.LinodeCluster
+}
