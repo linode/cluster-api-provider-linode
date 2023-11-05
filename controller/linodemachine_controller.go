351a192786a07c7f4794c18c2471a9c55368481f
Zhiwei Liang <zhi.wei.liang@outlook.com>
2023-11-05 02:08:50 -0500

    Fill up Reconcile function

--- controller/linodemachine_controller.go
+++ controller/linodemachine_controller.go
@@ -18,19 +18,25 @@ package controller
 
 import (
 	"context"
+	"fmt"
+	"time"
 
+	"github.com/linode/cluster-api-provider-linode/cloud/scope"
+	"github.com/linode/cluster-api-provider-linode/util/reconciler"
 	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/cluster-api/util"
+	"sigs.k8s.io/cluster-api/util/annotations"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
 
-	infrastructurev1alpha1 "github.com/linode/cluster-api-provider-linode/api/v1alpha1"
+	infrav1 "github.com/linode/cluster-api-provider-linode/api/v1alpha1"
 )
 
 // LinodeMachineReconciler reconciles a LinodeMachine object
 type LinodeMachineReconciler struct {
 	client.Client
-	Scheme *runtime.Scheme
+	Scheme           *runtime.Scheme
+	ReconcileTimeout time.Duration
 }
 
 //+kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=linodemachines,verbs=get;list;watch;create;update;patch;delete
@@ -47,16 +53,86 @@ type LinodeMachineReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.16.0/pkg/reconcile
 func (r *LinodeMachineReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	ctx, cancel := context.WithTimeout(ctx, reconciler.DefaultedLoopTimeout(r.ReconcileTimeout))
+	defer cancel()
+
+	log := ctrl.LoggerFrom(ctx)
 
 	// TODO(user): your logic here
+	linodeMachine := &infrav1.LinodeMachine{}
+	if err := r.Get(ctx, req.NamespacedName, linodeMachine); err != nil {
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
 
+	machine, err := util.GetOwnerMachine(ctx, r.Client, linodeMachine.ObjectMeta)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+	if machine == nil {
+		log.Info("Machine Controller has not yet set OwnerRef")
+		return ctrl.Result{}, nil
+	}
+
+	log = log.WithValues("Linode machine: ", machine.Name)
+	cluster, err := util.GetClusterFromMetadata(ctx, r.Client, machine.ObjectMeta)
+	if err != nil {
+		log.Info("Machine is missing cluster label or cluster does not exist")
+		return ctrl.Result{}, nil
+	}
+
+	linodeCluster := &infrav1.LinodeCluster{}
+	linodeClusterKey := client.ObjectKey{
+		Namespace: linodeMachine.Namespace,
+		Name:      cluster.Spec.InfrastructureRef.Name,
+	}
+
+	if err := r.Client.Get(ctx, linodeClusterKey, linodeCluster); err != nil {
+		log.Info("LinodeCluster is not available yet")
+		return ctrl.Result{}, nil
+	}
+
+	if annotations.IsPaused(cluster, linodeCluster) {
+		log.Info("LinodeMachine or linked Cluster is marked as paused. Won't reconcile")
+		return ctrl.Result{}, nil
+	}
+
+	clusterScope, err := scope.NewClusterScope(
+		ctx,
+		scope.ClusterScopeParams{
+			Client:        r.Client,
+			Cluster:       cluster,
+			LinodeCluster: linodeCluster,
+		},
+	)
+	if err != nil {
+		return ctrl.Result{}, fmt.Errorf("failed to create scope: %w", err)
+	}
+
+	machineScope, err := scope.NewMachineScope(
+		scope.MachineScopeParams{
+			Client:        r.Client,
+			Cluster:       cluster,
+			Machine:       machine,
+			LinodeCluster: linodeCluster,
+			LinodeMachine: linodeMachine,
+		},
+	)
+
+	return r.reconcile(ctx, machineScope, clusterScope)
+}
+
+func (r *LinodeMachineReconciler) reconcile(
+	ctx context.Context,
+	machineScope *scope.MachineScope,
+	clusterScope *scope.ClusterScope,
+) (ctrl.Result, error) {
+	// TODO
 	return ctrl.Result{}, nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *LinodeMachineReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&infrastructurev1alpha1.LinodeMachine{}).
+		For(&infrav1.LinodeMachine{}).
 		Complete(r)
 }
