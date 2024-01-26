7851a7ab5257d09b5cc6297d544224d3c9ec253d
Evan Johnson <ejohnson@akamai.com>
2024-01-26 09:51:31 -0500

    simplify case logic in loadbalancers.go

--- cloud/services/loadbalancers.go
+++ cloud/services/loadbalancers.go
@@ -34,15 +34,12 @@ func CreateNodeBalancer(ctx context.Context, clusterScope *scope.ClusterScope, l
 	if err != nil {
 		return nil, err
 	}
-	logger.Info("Creating NodeBalancer")
 	if linodeNBs, err = clusterScope.LinodeClient.ListNodeBalancers(ctx, linodego.NewListOptions(1, string(rawFilter))); err != nil {
 		logger.Info("Failed to list NodeBalancers", "error", err.Error())
 
 		return nil, err
 	}
-
-	switch len(linodeNBs) {
-	case 1:
+	if len(linodeNBs) == 1 {
 		logger.Info(fmt.Sprintf("NodeBalancer %s already exists", *linodeNBs[0].Label))
 		if !slices.Contains(linodeNBs[0].Tags, clusterUID) {
 			err = errors.New("NodeBalancer conflict")
@@ -50,46 +47,32 @@ func CreateNodeBalancer(ctx context.Context, clusterScope *scope.ClusterScope, l
 
 			return nil, err
 		}
-		linodeNB = &linodeNBs[0]
-	case 0:
-		logger.Info(fmt.Sprintf("Creating NodeBalancer %s-api-server", clusterScope.LinodeCluster.Name))
-		createConfig := linodego.NodeBalancerCreateOptions{
-			Label:              util.Pointer(fmt.Sprintf("%s-api-server", clusterScope.LinodeCluster.Name)),
-			Region:             clusterScope.LinodeCluster.Spec.Region,
-			ClientConnThrottle: nil,
-			Tags:               tags,
-		}
 
-		if linodeNB, err = clusterScope.LinodeClient.CreateNodeBalancer(ctx, createConfig); err != nil {
-			logger.Info("Failed to create Linode NodeBalancer", "error", err.Error())
-
-			// Already exists is not an error
-			apiErr := linodego.Error{}
-			if errors.As(err, &apiErr) && apiErr.Code != http.StatusFound {
-				return nil, err
-			}
-
-			err = nil
-
-			if linodeNB != nil {
-				logger.Info("Linode NodeBalancer already exists", "existing", linodeNB.Label)
-			}
-		}
-
-	default:
-		err = errors.New("multiple NodeBalancers")
-
-		logger.Error(err, "Panic! Multiple NodeBalancers found. This might be a concurrency issue in the controller!!!", "filters", string(rawFilter))
-
-		return nil, err
+		return &linodeNBs[0], nil
 	}
 
-	if linodeNB == nil {
-		err = errors.New("missing NodeBalancer")
+	logger.Info(fmt.Sprintf("Creating NodeBalancer %s-api-server", clusterScope.LinodeCluster.Name))
+	createConfig := linodego.NodeBalancerCreateOptions{
+		Label:              util.Pointer(fmt.Sprintf("%s-api-server", clusterScope.LinodeCluster.Name)),
+		Region:             clusterScope.LinodeCluster.Spec.Region,
+		ClientConnThrottle: nil,
+		Tags:               tags,
+	}
 
-		logger.Error(err, "Panic! Failed to create NodeBalancer")
+	if linodeNB, err = clusterScope.LinodeClient.CreateNodeBalancer(ctx, createConfig); err != nil {
+		logger.Info("Failed to create Linode NodeBalancer", "error", err.Error())
 
-		return nil, err
+		// Already exists is not an error
+		apiErr := linodego.Error{}
+		if errors.As(err, &apiErr) && apiErr.Code != http.StatusFound {
+			return nil, err
+		}
+
+		err = nil
+
+		if linodeNB != nil {
+			logger.Info("Linode NodeBalancer already exists", "existing", linodeNB.Label)
+		}
 	}
 
 	return linodeNB, nil
@@ -106,33 +89,24 @@ func CreateNodeBalancerConfig(ctx context.Context, clusterScope *scope.ClusterSc
 
 		return nil, err
 	}
+	if len(linodeNBConfigs) == 1 {
+		logger.Info("NodeBalancer ", strconv.Itoa(linodeNBConfigs[0].ID), " already exists")
+
+		return &linodeNBConfigs[0], err
+	}
 	lbPort := defaultLBPort
 	if clusterScope.LinodeCluster.Spec.Network.LoadBalancerPort != 0 {
 		lbPort = clusterScope.LinodeCluster.Spec.Network.LoadBalancerPort
 	}
-	switch len(linodeNBConfigs) {
-	case 1:
-		logger.Info("NodeBalancer ", strconv.Itoa(linodeNBConfigs[0].ID), " already exists")
-		linodeNBConfig = &linodeNBConfigs[0]
+	createConfig := linodego.NodeBalancerConfigCreateOptions{
+		Port:      lbPort,
+		Protocol:  linodego.ProtocolTCP,
+		Algorithm: linodego.AlgorithmRoundRobin,
+		Check:     linodego.CheckConnection,
+	}
 
-	case 0:
-		createConfig := linodego.NodeBalancerConfigCreateOptions{
-			Port:      lbPort,
-			Protocol:  linodego.ProtocolTCP,
-			Algorithm: linodego.AlgorithmRoundRobin,
-			Check:     linodego.CheckConnection,
-		}
-
-		if linodeNBConfig, err = clusterScope.LinodeClient.CreateNodeBalancerConfig(ctx, clusterScope.LinodeCluster.Spec.Network.NodeBalancerID, createConfig); err != nil {
-			logger.Info("Failed to create Linode NodeBalancer config", "error", err.Error())
-
-			return nil, err
-		}
-
-	default:
-		err = errors.New("multiple NodeBalancer Configs")
-
-		logger.Error(err, "Panic! Multiple NodeBalancer Configs found. This might be a concurrency issue in the controller!!!")
+	if linodeNBConfig, err = clusterScope.LinodeClient.CreateNodeBalancerConfig(ctx, clusterScope.LinodeCluster.Spec.Network.NodeBalancerID, createConfig); err != nil {
+		logger.Info("Failed to create Linode NodeBalancer config", "error", err.Error())
 
 		return nil, err
 	}
