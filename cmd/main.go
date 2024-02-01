047ad7b3b5236a7ce4618b33147a66522addb8ea
Evan Johnson <ejohnson@akamai.com>
2024-02-01 14:33:58 -0500

    update CRD creation for VPC and setup VPC Controller in main.go

--- cmd/main.go
+++ cmd/main.go
@@ -123,6 +123,15 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "LinodeMachine")
 		os.Exit(1)
 	}
+	if err = (&controller2.LinodeVPCReconciler{
+		Client:           mgr.GetClient(),
+		Recorder:         mgr.GetEventRecorderFor("LinodeVPCReconciler"),
+		WatchFilterValue: clusterWatchFilter,
+		LinodeApiKey:     linodeToken,
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "LinodeVPC")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
