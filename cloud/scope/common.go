7265b5b138ab89b4af0caa62f3670ad66b6b01b6
Richard Kovacs <rkovacs@akamai.com>
2024-02-13 15:43:26 +0100

    Cache Linode API GET calls

--- cloud/scope/common.go
+++ cloud/scope/common.go
@@ -1,21 +1,109 @@
 package scope
 
 import (
+	"fmt"
 	"net/http"
+	"sync"
+	"time"
 
+	"github.com/bxcodec/gotcha"
+	"github.com/bxcodec/gotcha/cache"
+	"github.com/bxcodec/httpcache"
+	"github.com/bxcodec/httpcache/cache/inmem"
 	"github.com/linode/linodego"
 	"golang.org/x/oauth2"
 )
 
-func createLinodeClient(apiKey string) *linodego.Client {
-	tokenSource := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: apiKey})
+const (
+	// DefaultLinodeAPITimeout is the default timeout for Linode API calls.
+	DefaultLinodeAPITimeout = 10 * time.Second
+	// DefaultLinodeAPIMaxIdleConns is the default max idle connections for Linode API calls.
+	DefaultLinodeAPIMaxIdleConns = 100
+	// DefaultLinodeAPICacheControlOverride is the default cache control override for Linode API calls.
+	DefaultLinodeAPICacheControlOverride = "max-age=60"
+	// DefaultLinodeAPICacheExpiration is the default cache expiration for Linode API calls.
+	DefaultLinodeAPICacheExpiration = 5 * time.Second
+	// DefaultLinodeAPICacheMaxSizeItem is the default cache max size item for Linode API calls.
+	DefaultLinodeAPICacheMaxSizeItem = 500
+	// DefaultLinodeAPICacheMaxMemory is the default cache max memory for Linode API calls.
+	DefaultLinodeAPICacheMaxMemory = cache.MB
+)
 
-	oauth2Client := &http.Client{
-		Transport: &oauth2.Transport{
-			Source: tokenSource,
-		},
-	}
-	linodeClient := linodego.NewClient(oauth2Client)
+var (
+	initClient   sync.Once
+	linodeClient linodego.Client
+)
+
+func createLinodeClient(apiKey string) *linodego.Client {
+	initClient.Do(func() {
+		//nolint:forcetypeassert // Always OK.
+		baseTrans := http.DefaultTransport.(*http.Transport).Clone()
+		baseTrans.MaxIdleConns = DefaultLinodeAPIMaxIdleConns
+		// Otherwise we use just a few connections.
+		baseTrans.MaxConnsPerHost = baseTrans.MaxIdleConns
+		baseTrans.MaxIdleConnsPerHost = baseTrans.MaxIdleConns
+
+		oauthTrans := &oauth2.Transport{
+			Source: oauth2.StaticTokenSource(&oauth2.Token{AccessToken: apiKey}),
+			Base:   baseTrans,
+		}
+
+		oauth2Client := &http.Client{
+			Timeout:   DefaultLinodeAPITimeout,
+			Transport: oauthTrans,
+		}
+
+		cacheStore := gotcha.New(&cache.Option{
+			AlgorithmType: cache.LRUAlgorithm,
+			ExpiryTime:    DefaultLinodeAPICacheExpiration,
+			MaxSizeItem:   DefaultLinodeAPICacheMaxSizeItem,
+			MaxMemory:     DefaultLinodeAPICacheMaxMemory,
+		})
+		cachedTrans := httpcache.NewCacheHandlerRoundtrip(&maxAgeFixRoundTripper{
+			oauthTrans,
+		}, false, inmem.NewCache(cacheStore))
+
+		oauth2Client.Transport = &reqURIFixRoundTripper{
+			plain:  oauthTrans,
+			cached: cachedTrans,
+		}
+
+		linodeClient = linodego.NewClient(oauth2Client)
+	})
 
 	return &linodeClient
+}
+
+type maxAgeFixRoundTripper struct {
+	http.RoundTripper
+}
+
+func (r *maxAgeFixRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
+	resp, err := r.RoundTripper.RoundTrip(req)
+	if err == nil && req.Method == http.MethodGet {
+		// Workaround, because Linode API sends max-age=0 all the time.
+		resp.Header.Set("Cache-Control", DefaultLinodeAPICacheControlOverride)
+	}
+
+	return resp, err
+}
+
+type reqURIFixRoundTripper struct {
+	plain  http.RoundTripper
+	cached http.RoundTripper
+}
+
+func (r *reqURIFixRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
+	if req.Method != http.MethodGet {
+		return r.plain.RoundTrip(req)
+	}
+
+	// Workaround because Linodego doesn't set it.
+	req.RequestURI = req.URL.Path
+
+	if filter := req.Header.Get("X-Filter"); filter != "" {
+		req.RequestURI = fmt.Sprintf("%s?filter=%s", req.RequestURI, filter)
+	}
+
+	return r.cached.RoundTrip(req)
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.21
 toolchain go1.21.5
 
 require (
+	github.com/bxcodec/httpcache v1.0.0-beta.3
 	github.com/go-logr/logr v1.4.1
 	github.com/google/uuid v1.3.1
 	github.com/linode/linodego v1.28.0
@@ -23,6 +24,7 @@ require (
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/blang/semver/v4 v4.0.0 // indirect
+	github.com/bxcodec/gotcha v1.0.0-beta.2 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/emicklei/go-restful/v3 v3.11.0 // indirect
