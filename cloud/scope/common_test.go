2c49d12631fe86b491227293429caeba68a1c86f
Khaja Omer <komer@akamai.com>
2024-03-06 12:28:41 -0600

    Add additional test cases to get max test coverage for common.go

--- cloud/scope/common_test.go
+++ cloud/scope/common_test.go
@@ -2,6 +2,7 @@ package scope
 
 import (
 	"context"
+	"errors"
 	"testing"
 
 	"github.com/linode/cluster-api-provider-linode/cloud/scope/mock"
@@ -43,7 +44,8 @@ func Test_createLinodeClient(t *testing.T) {
 func Test_getCredentialDataFromRef(t *testing.T) {
 	type args struct {
 		ctx              context.Context
-		credentialsRef   corev1.SecretReference
+		providedCredentialsRef   corev1.SecretReference
+		expectedCredentialsRef   corev1.SecretReference
 		defaultNamespace string
 		funcBehavior     func(ctx context.Context, key types.NamespacedName, obj *corev1.Secret, opts ...client.GetOption) error
 	}
@@ -54,10 +56,14 @@ func Test_getCredentialDataFromRef(t *testing.T) {
 		expectedError string 
 	}{
 		{
-			name: "Simple test case if no error",
+			name: "Check is the function works correctly",
 			args: args{
 				ctx: context.Background(),
-				credentialsRef: corev1.SecretReference{
+				providedCredentialsRef: corev1.SecretReference{
+					Name:      "example",
+					Namespace: "test",
+				},
+				expectedCredentialsRef: corev1.SecretReference{
 					Name:      "example",
 					Namespace: "test",
 				},
@@ -75,6 +81,72 @@ func Test_getCredentialDataFromRef(t *testing.T) {
 			expectedByte: []byte("example"),
 			expectedError: "",
 		},
+		{
+			name: "Empty namespace test case",
+			args: args{
+				ctx: context.Background(),
+				providedCredentialsRef: corev1.SecretReference{
+					Name:      "example",
+					Namespace: "",
+				},
+				expectedCredentialsRef: corev1.SecretReference{
+					Name:      "example",
+					Namespace: "default",
+				},
+				defaultNamespace: "default",
+				funcBehavior: func(ctx context.Context, key types.NamespacedName, obj *corev1.Secret, opts ...client.GetOption) error {
+					cred := corev1.Secret{
+						Data: map[string][]byte{
+							"apiToken": []byte("example"),
+						},
+					}
+					*obj = cred
+					return nil
+				},
+			},
+			expectedByte: []byte("example"),
+			expectedError: "",
+		},
+		{
+			name: "Handle error from crClient",
+			args: args{
+				ctx: context.Background(),
+				providedCredentialsRef: corev1.SecretReference{
+					Name:      "example",
+					Namespace: "test",
+				},
+				expectedCredentialsRef: corev1.SecretReference{
+					Name:      "example",
+					Namespace: "test",
+				},
+				defaultNamespace: "default",
+				funcBehavior: func(ctx context.Context, key types.NamespacedName, obj *corev1.Secret, opts ...client.GetOption) error {
+					return errors.New("Could not find the secret")
+				},
+			},
+			expectedByte: []byte(nil),
+			expectedError: "get credentials secret test/example: Could not find the secret",
+		},
+		{
+			name: "Handle error after getting empty secret from crClient",
+			args: args{
+				ctx: context.Background(),
+				providedCredentialsRef: corev1.SecretReference{
+					Name:      "example",
+					Namespace: "test",
+				},
+				expectedCredentialsRef: corev1.SecretReference{
+					Name:      "example",
+					Namespace: "test",
+				},
+				defaultNamespace: "default",
+				funcBehavior: func(ctx context.Context, key types.NamespacedName, obj *corev1.Secret, opts ...client.GetOption) error {
+					return nil
+				},
+			},
+			expectedByte: []byte(nil),
+			expectedError: "no apiToken key in credentials secret test/example",
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -85,14 +157,14 @@ func Test_getCredentialDataFromRef(t *testing.T) {
 			mockClient := mock.NewMockcrClient(ctrl)
 
 			// Setup Expected behaviour
-			secretRef := client.ObjectKey{
-				Name:      tt.args.credentialsRef.Name,
-				Namespace: tt.args.credentialsRef.Namespace,
+			expectedSecretRef := client.ObjectKey{
+				Name:      tt.args.expectedCredentialsRef.Name,
+				Namespace: tt.args.expectedCredentialsRef.Namespace,
 			}
-			mockClient.EXPECT().Get(gomock.Any(), secretRef, gomock.Any()).DoAndReturn(tt.args.funcBehavior)
+			mockClient.EXPECT().Get(gomock.Any(), expectedSecretRef, gomock.Any()).DoAndReturn(tt.args.funcBehavior)
 
 			// Call the function under test with the mock client
-			got, err := getCredentialDataFromRef(tt.args.ctx, mockClient, tt.args.credentialsRef, tt.args.defaultNamespace)
+			got, err := getCredentialDataFromRef(tt.args.ctx, mockClient, tt.args.providedCredentialsRef, tt.args.defaultNamespace)
 
 			// Check that the function returned the expected result
 			if tt.expectedError != "" {
