4a8741ded0f932da852489d46848d87e0dff78ab
Khaja Omer <56000175+komer3@users.noreply.github.com>
2024-03-11 15:32:14 -0500

    Add Unit tests for cloud/scope/cluster.go (#176)
    ---------
    
    Co-authored-by: Khaja Omer <komer@akamai.com>

--- cloud/scope/client.go
+++ cloud/scope/client.go
@@ -1,10 +1,16 @@
 package scope
 
 import (
+	"context"
+
+	"sigs.k8s.io/cluster-api/util/patch"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 type k8sClient interface {
-	client.Reader
-	client.Writer
+	client.Client
+}
+
+type PatchHelper interface {
+	Patch(ctx context.Context, obj client.Object, opts ...patch.Option) error
 }
