d831767df94148a94eafdd277ff65a76ea627ec3
Khaja Omer <komer@akamai.com>
2024-03-19 11:08:12 -0500

    saving

--- cloud/services/object_storage_buckets_test.go
+++ cloud/services/object_storage_buckets_test.go
@@ -5,32 +5,69 @@ import (
 	"reflect"
 	"testing"
 
+	infrav1alpha1 "github.com/linode/cluster-api-provider-linode/api/v1alpha1"
 	"github.com/linode/cluster-api-provider-linode/cloud/scope"
+	"github.com/linode/cluster-api-provider-linode/mock"
 	"github.com/linode/linodego"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/mock/gomock"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func TestEnsureObjectStorageBucket(t *testing.T) {
-	type args struct {
-		ctx    context.Context
-		bScope *scope.ObjectStorageBucketScope
-	}
+	t.Parallel()
+
 	tests := []struct {
 		name    string
-		args    args
+		bScope *scope.ObjectStorageBucketScope
 		want    *linodego.ObjectStorageBucket
-		wantErr bool
+		expectedError error
+		expects func(mock *mock.MockLinodeClient)
 	}{
-		// TODO: Add test cases.
+		{
+			name: "Success - Successfully get the OBJ bucket",
+			bScope: &scope.ObjectStorageBucketScope{
+				Bucket: &infrav1alpha1.LinodeObjectStorageBucket{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "test-bucket",
+					},
+					Spec: infrav1alpha1.LinodeObjectStorageBucketSpec{
+						Cluster: "test-cluster",
+					},
+				},
+			},
+			want: &linodego.ObjectStorageBucket{
+				Label: "test-bucket",
+				
+			},
+			expects: func(c *mock.MockLinodeClient) {
+				c.EXPECT().GetObjectStorageBucket(gomock.Any(), gomock.Any(), gomock.Any()).Return(&linodego.ObjectStorageBucket{
+					Label: "test-bucket",
+
+				}, nil)	
+			},
+			expectedError: nil,
+		},
 	}
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := EnsureObjectStorageBucket(tt.args.ctx, tt.args.bScope)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("EnsureObjectStorageBucket() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("EnsureObjectStorageBucket() = %v, want %v", got, tt.want)
+		testcase := tt
+		t.Run(testcase.name, func(t *testing.T) {
+			t.Parallel()
+
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			mockClient := mock.NewMockLinodeClient(ctrl)
+
+			testcase.bScope.LinodeClient = mockClient
+
+			testcase.expects(mockClient)
+
+			got, err := EnsureObjectStorageBucket(context.Background(), testcase.bScope)
+			if testcase.expectedError != nil {
+				assert.ErrorContains(t, err, testcase.expectedError.Error())
+			} else {
+				assert.Equal(t, testcase.want, got)
 			}
 		})
 	}
