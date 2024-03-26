60f6c60231bc0f3da246dc1798fbddabcc18c6ad
Ashley Dumaine <5779804+AshleyDumaine@users.noreply.github.com>
2024-03-26 09:50:38 -0400

    wait up to 30 seconds for root disk to resize (#212)

--- cloud/scope/client.go
+++ cloud/scope/client.go
@@ -28,6 +28,7 @@ type LinodeInstanceClient interface {
 	CreateInstanceDisk(ctx context.Context, linodeID int, opts linodego.InstanceDiskCreateOptions) (*linodego.InstanceDisk, error)
 	GetInstance(ctx context.Context, linodeID int) (*linodego.Instance, error)
 	DeleteInstance(ctx context.Context, linodeID int) error
+	WaitForInstanceDiskStatus(ctx context.Context, instanceID int, diskID int, status linodego.DiskStatus, timeoutSeconds int) (*linodego.InstanceDisk, error)
 }
 
 // LinodeVPCClient defines the methods that a Linode client must have to interact with Linode's VPC service.
