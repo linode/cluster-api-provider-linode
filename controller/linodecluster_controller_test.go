bcb40c2d9912d4e1a83f148dddd9e34e2228cbc1
Amol Deodhar <adeodhar@akamai.com>
2024-04-09 13:07:35 -0400

    Add non-happy-path tests

--- controller/linodecluster_controller_test.go
+++ controller/linodecluster_controller_test.go
@@ -125,3 +125,233 @@ var _ = Describe("lifecycle", Ordered, Label("cluster", "lifecycle"), func() {
 		Expect(linodeCluster.Spec.ControlPlaneEndpoint.Host).To(Equal(controlPlaneEndpointHost))
 	})
 })
+
+var _ = Describe("no-capl-cluster", Ordered, Label("cluster", "no-capl-cluster"), func() {
+	var mockCtrl *gomock.Controller
+	var reconciler *LinodeClusterReconciler
+	nodebalancerID := 1
+	controlPlaneEndpointHost := "10.0.0.1"
+	clusterName := "no-capl-cluster"
+	clusterNameSpace := "default"
+	metadata := metav1.ObjectMeta{
+		Name:      clusterName,
+		Namespace: clusterNameSpace,
+	}
+
+	linodeCluster := infrav1.LinodeCluster{
+		ObjectMeta: metadata,
+		Spec: infrav1.LinodeClusterSpec{
+			Region: "us-ord",
+			Network: infrav1.NetworkSpec{
+				NodeBalancerID: &nodebalancerID,
+			},
+			ControlPlaneEndpoint: clusterv1.APIEndpoint{
+				Host: controlPlaneEndpointHost,
+			},
+		},
+	}
+
+	// Create a recorder with a buffered channel for consuming event strings.
+	recorder := record.NewFakeRecorder(10)
+
+	BeforeEach(func() {
+		// Create a new gomock controller for each test run
+		mockCtrl = gomock.NewController(GinkgoT())
+		reconciler = &LinodeClusterReconciler{
+			Client:       k8sClient,
+			Recorder:     recorder,
+			LinodeApiKey: "test-key",
+		}
+	})
+
+	AfterEach(func() {
+		// At the end of each test run, tell the gomock controller it's done
+		// so it can check configured expectations and validate the methods called
+		mockCtrl.Finish()
+		// Flush the channel if any events were not consumed.
+		for len(recorder.Events) > 0 {
+			<-recorder.Events
+		}
+	})
+
+	It("should fail reconciliation if no capl cluster exists", func(ctx SpecContext) {
+		clusterKey := client.ObjectKeyFromObject(&linodeCluster)
+		Expect(k8sClient.Create(ctx, &linodeCluster)).To(Succeed())
+		_, err := reconciler.Reconcile(ctx, reconcile.Request{
+			NamespacedName: clusterKey,
+		})
+		Expect(err).NotTo(HaveOccurred())
+
+		By("checking ready conditions")
+		Expect(k8sClient.Get(ctx, clusterKey, &linodeCluster)).To(Succeed())
+		Expect(linodeCluster.Status.Ready).To(BeFalseBecause("failed to get Cluster/no-capl-cluster: clusters.cluster.x-k8s.io \"no-capl-cluster\" not found"))
+	})
+})
+
+var _ = Describe("no-owner-ref", Ordered, Label("cluster", "no-owner-ref"), func() {
+	var mockCtrl *gomock.Controller
+	var reconciler *LinodeClusterReconciler
+	nodebalancerID := 1
+	controlPlaneEndpointHost := "10.0.0.1"
+	clusterName := "no-owner-ref"
+	clusterNameSpace := "default"
+	metadata := metav1.ObjectMeta{
+		Name:      clusterName,
+		Namespace: clusterNameSpace,
+	}
+
+	caplCluster := clusterv1.Cluster{
+		ObjectMeta: metadata,
+		Spec: clusterv1.ClusterSpec{
+			InfrastructureRef: &corev1.ObjectReference{
+				Kind:      "LinodeCluster",
+				Name:      clusterName,
+				Namespace: clusterNameSpace,
+			},
+			ControlPlaneRef: &corev1.ObjectReference{
+				Kind:      "KubeadmControlPlane",
+				Name:      "lifecycle-control-plane",
+				Namespace: clusterNameSpace,
+			},
+		},
+	}
+
+	linodeCluster := infrav1.LinodeCluster{
+		ObjectMeta: metadata,
+		Spec: infrav1.LinodeClusterSpec{
+			Region: "us-ord",
+			Network: infrav1.NetworkSpec{
+				NodeBalancerID: &nodebalancerID,
+			},
+			ControlPlaneEndpoint: clusterv1.APIEndpoint{
+				Host: controlPlaneEndpointHost,
+			},
+		},
+	}
+
+	// Create a recorder with a buffered channel for consuming event strings.
+	recorder := record.NewFakeRecorder(10)
+
+	BeforeEach(func() {
+		// Create a new gomock controller for each test run
+		mockCtrl = gomock.NewController(GinkgoT())
+		reconciler = &LinodeClusterReconciler{
+			Client:       k8sClient,
+			Recorder:     recorder,
+			LinodeApiKey: "test-key",
+		}
+	})
+
+	AfterEach(func() {
+		// At the end of each test run, tell the gomock controller it's done
+		// so it can check configured expectations and validate the methods called
+		mockCtrl.Finish()
+		// Flush the channel if any events were not consumed.
+		for len(recorder.Events) > 0 {
+			<-recorder.Events
+		}
+	})
+
+	It("linode cluster should remain NotReady", func(ctx SpecContext) {
+		clusterKey := client.ObjectKeyFromObject(&linodeCluster)
+		Expect(k8sClient.Create(ctx, &caplCluster)).To(Succeed())
+		Expect(k8sClient.Create(ctx, &linodeCluster)).To(Succeed())
+		_, err := reconciler.Reconcile(ctx, reconcile.Request{
+			NamespacedName: clusterKey,
+		})
+		Expect(err).NotTo(HaveOccurred())
+
+		By("checking ready conditions")
+		Expect(k8sClient.Get(ctx, clusterKey, &linodeCluster)).To(Succeed())
+		Expect(linodeCluster.Status.Ready).To(BeFalse())
+		Expect(linodeCluster.Status.FailureMessage).To(BeNil())
+		Expect(linodeCluster.Status.FailureReason).To(BeNil())
+	})
+})
+
+var _ = Describe("no-ctrl-plane-endpt", Ordered, Label("cluster", "no-ctrl-plane-endpt"), func() {
+	var mockCtrl *gomock.Controller
+	var reconciler *LinodeClusterReconciler
+	clusterName := "no-ctrl-plane-endpt"
+	clusterNameSpace := "default"
+	ownerRef := metav1.OwnerReference{
+		Name:       clusterName,
+		APIVersion: "cluster.x-k8s.io/v1beta1",
+		Kind:       "Cluster",
+		UID:        "00000000-000-0000-0000-000000000000",
+	}
+	ownerRefs := []metav1.OwnerReference{ownerRef}
+	metadata := metav1.ObjectMeta{
+		Name:            clusterName,
+		Namespace:       clusterNameSpace,
+		OwnerReferences: ownerRefs,
+	}
+
+	caplCluster := clusterv1.Cluster{
+		ObjectMeta: metadata,
+		Spec: clusterv1.ClusterSpec{
+			InfrastructureRef: &corev1.ObjectReference{
+				Kind:      "LinodeCluster",
+				Name:      clusterName,
+				Namespace: clusterNameSpace,
+			},
+			ControlPlaneRef: &corev1.ObjectReference{
+				Kind:      "KubeadmControlPlane",
+				Name:      "lifecycle-control-plane",
+				Namespace: clusterNameSpace,
+			},
+		},
+	}
+
+	linodeCluster := infrav1.LinodeCluster{
+		ObjectMeta: metadata,
+		Spec: infrav1.LinodeClusterSpec{
+			Region: "us-ord",
+		},
+	}
+
+	// Create a recorder with a buffered channel for consuming event strings.
+	recorder := record.NewFakeRecorder(10)
+
+	BeforeEach(func() {
+		// Create a new gomock controller for each test run
+		mockCtrl = gomock.NewController(GinkgoT())
+		reconciler = &LinodeClusterReconciler{
+			Client:       k8sClient,
+			Recorder:     recorder,
+			LinodeApiKey: "test-key",
+		}
+	})
+
+	AfterEach(func() {
+		// At the end of each test run, tell the gomock controller it's done
+		// so it can check configured expectations and validate the methods called
+		mockCtrl.Finish()
+		// Flush the channel if any events were not consumed.
+		for len(recorder.Events) > 0 {
+			<-recorder.Events
+		}
+	})
+
+	It("should fail creating cluster", func(ctx SpecContext) {
+		clusterKey := client.ObjectKeyFromObject(&linodeCluster)
+		Expect(k8sClient.Create(ctx, &caplCluster)).To(Succeed())
+		Expect(k8sClient.Create(ctx, &linodeCluster)).To(Succeed())
+		_, err := reconciler.Reconcile(ctx, reconcile.Request{
+			NamespacedName: clusterKey,
+		})
+		Expect(err).To(HaveOccurred())
+
+		By("checking ready conditions")
+		Expect(k8sClient.Get(ctx, clusterKey, &linodeCluster)).To(Succeed())
+		Expect(linodeCluster.Status.Ready).To(BeFalse())
+		Expect(linodeCluster.Status.Conditions).To(HaveLen(1))
+		Expect(linodeCluster.Status.Conditions[0].Type).To(Equal(clusterv1.ReadyCondition))
+
+		By("checking controlPlaneEndpoint host")
+		Expect(linodeCluster.Spec.ControlPlaneEndpoint.Host).To(Equal(""))
+
+		By("checking nb id to be nil")
+		Expect(linodeCluster.Spec.Network.NodeBalancerID).To(BeNil())
+	})
+})
