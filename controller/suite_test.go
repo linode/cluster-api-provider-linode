d25603faab96dd7053616d05dcb83926486945bf
amold1 <amold1@gmail.com>
2024-04-12 23:15:56 -0400

    Add tests for LinodeCluster controller (#247)
    * Add tests for Linode Cluster Controller
    * Install CAPI CRDs for tests

--- controller/suite_test.go
+++ controller/suite_test.go
@@ -18,10 +18,14 @@ package controller
 
 import (
 	"fmt"
+	"go/build"
+	"os"
 	"path/filepath"
 	"runtime"
+	"strings"
 	"testing"
 
+	"golang.org/x/mod/modfile"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -30,6 +34,8 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
 	infrav1 "github.com/linode/cluster-api-provider-linode/api/v1alpha1"
+	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
+
 	//+kubebuilder:scaffold:imports
 
 	. "github.com/onsi/ginkgo/v2"
@@ -40,9 +46,16 @@ import (
 // These tests use Ginkgo (BDD-style Go testing framework). Refer to
 // http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
 
-var cfg *rest.Config
-var k8sClient client.Client
-var testEnv *envtest.Environment
+var (
+	cfg               *rest.Config
+	k8sClient         client.Client
+	testEnv           *envtest.Environment
+	clusterAPIVersion string
+	data              []byte
+	err               error
+	_, b, _, _        = runtime.Caller(0)
+	basepath          = filepath.Dir(b)
+)
 
 func TestControllers(t *testing.T) {
 	t.Parallel()
@@ -52,12 +65,57 @@ func TestControllers(t *testing.T) {
 	RunSpecs(t, "Controller Suite")
 }
 
+func getFilePathToCAPICRDs() string {
+	goModFilePath := filepath.Join(basepath, "..", "go.mod")
+
+	// Read the go.mod file
+	data, err = os.ReadFile(goModFilePath)
+	if err != nil {
+		panic(err)
+	}
+
+	// Parse the file
+	parsedFile, err := modfile.Parse(goModFilePath, data, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	// Get the cluster-api version
+	for _, goMod := range parsedFile.Require {
+		if strings.Contains(goMod.Mod.Path, "cluster-api") {
+			clusterAPIVersion = goMod.Mod.Version
+		}
+	}
+
+	if clusterAPIVersion == "" {
+		panic("Could not find cluster-api version in the go.mod file")
+	}
+
+	gopath := envOr("GOPATH", build.Default.GOPATH)
+	return filepath.Join(gopath, "pkg", "mod", "sigs.k8s.io", fmt.Sprintf("cluster-api@%s", clusterAPIVersion), "config", "crd", "bases")
+}
+
+func envOr(envKey, defaultValue string) string {
+	if value, ok := os.LookupEnv(envKey); ok {
+		return value
+	}
+	return defaultValue
+}
+
 var _ = BeforeSuite(func() {
 	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
 
+	crdPaths := []string{
+		filepath.Join("..", "config", "crd", "bases"),
+	}
+
+	if capiPath := getFilePathToCAPICRDs(); capiPath != "" {
+		crdPaths = append(crdPaths, capiPath)
+	}
+
 	By("bootstrapping test environment")
 	testEnv = &envtest.Environment{
-		CRDDirectoryPaths:     []string{filepath.Join("..", "config", "crd", "bases")},
+		CRDDirectoryPaths:     crdPaths,
 		ErrorIfCRDPathMissing: true,
 
 		// The BinaryAssetsDirectory is only required if you want to run the tests directly
@@ -76,6 +134,7 @@ var _ = BeforeSuite(func() {
 	Expect(cfg).NotTo(BeNil())
 
 	Expect(infrav1.AddToScheme(scheme.Scheme)).To(Succeed())
+	Expect(clusterv1.AddToScheme(scheme.Scheme)).To(Succeed())
 
 	//+kubebuilder:scaffold:scheme
 
