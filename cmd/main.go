848f40aec1d62c98f842ec0e41fa500e7e0aa191
Cindy Bang <cbang@akamai.com>
2024-05-08 09:00:00 -0400

    feat: linodevpc: kubebuilder create webhook
    Scaffold a validating admission webhook for the LinodeVPC resource
    with Kubebuider via the command:
    kubebuilder create webhook --group infrastructure --version v1alpha1 --kind LinodeVPC --programmatic-validation

--- cmd/main.go
+++ cmd/main.go
@@ -159,6 +159,12 @@ func main() {
 			os.Exit(1)
 		}
 	}
+	if os.Getenv("ENABLE_WEBHOOKS") != "false" {
+		if err = (&infrastructurev1alpha1.LinodeVPC{}).SetupWebhookWithManager(mgr); err != nil {
+			setupLog.Error(err, "unable to create webhook", "webhook", "LinodeVPC")
+			os.Exit(1)
+		}
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
