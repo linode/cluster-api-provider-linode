d2fa20770fbe7f923fa6d9e8ad6ead758ace0e9b
Amol Deodhar <adeodhar@akamai.com>
2024-05-13 20:30:48 -0400

    script

--- hack/generate-flavors.sh
+++ hack/generate-flavors.sh
@@ -3,28 +3,26 @@
 set -euo pipefail
 
 REPO_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
-STANDARD_ADDONS_DIR="${REPO_ROOT}/templates/addons"
-DISTRO_DIR="${REPO_ROOT}/templates/distros"
-
+FLAVORS_DIR="${REPO_ROOT}/templates/flavors"
 SUPPORTED_DISTROS=(
+    "clusterclass-kubeadm"
     "rke2"
     "k3s"
     "kubeadm"
 )
 
-for distro in SUPPORTED_DISTROS; do
-    paths=()
-    kustomize_file_path="kustomization.yaml"
-    paths+=(${DISTRO_DIR}/${distro}/${kustomize_file_path})
-    for addon in $(find "${STANDARD_ADDONS_DIR}/"* -maxdepth 0 -type d -print0 | xargs -0 -I {} basename {}); do
-        paths+=(${STANDARD_ADDONS_DIR}/${addon}/${kustomize_file_path})
+for distro in ${SUPPORTED_DISTROS[@]}; do
+    if [[ $distro == "clusterclass-kubeadm" ]]; then
+        # clusterctl expects clusterclass not have the "cluster-template" prefix
+        # except for the actual cluster template using the clusterclass
+        echo "****** Generating clusterclass-kubeadm flavor ******"
+        kustomize build "${FLAVORS_DIR}/${distro}" > "${REPO_ROOT}/templates/${distro}.yaml"
+        cp "${FLAVORS_DIR}/${distro}/cluster-template.yaml" "${REPO_ROOT}/templates/cluster-template-${distro}.yaml"
+        continue
+    fi
+
+    for name in $(find "${FLAVORS_DIR}/${distro}/"* -maxdepth 0 -type d -print0 | xargs -0 -I {} basename {}); do
+        echo "****** Generating ${distro}-${name} flavor ******"
+        kustomize build "${FLAVORS_DIR}/${distro}/${name}" > "${REPO_ROOT}/templates/cluster-template-${distro}-${name}.yaml"
     done
-    spruce merge --fallback-append ${paths} > kustomization.yaml
 done
-
-kustomize build "${STANDARD_ADDONS_DIR}/${addon}" > "${REPO_ROOT}/templates/cluster-template-${distro}-standard.yaml"
-
-
-# move the default template to the default file expected by clusterctl
-mv "${REPO_ROOT}/templates/cluster-template-default.yaml" "${REPO_ROOT}/templates/cluster-template.yaml"
-
