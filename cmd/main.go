db9eeda927c562682e0b515c9dd0edee95f47a2f
cbzzz <69888673+cbzzz@users.noreply.github.com>
2024-05-15 10:16:17 -0400

    feat: linodemachine: add validating admission webhook on create (#291)
    * fixup! Rename csi-driver to better short-name. (#158)
    
    * chore: add kubebuilder
    
    * fixup! shorten cluster-api-provider-linode to capl, add release process (#124)
    
    * feat: linodemachine: kubebuilder create webhook
    
    Scaffold a validating admission webhook for the LinodeMachine resource
    with Kubebuider via the command:
    kubebuilder create webhook --group infrastructure --version v1alpha1 --kind LinodeMachine --programmatic-validation
    
    * fixup! feat: linodemachine: kubebuilder create webhook
    
    * tiltfile: add validation webhook
    
    * chore: e2e: use 6th Generation Linode plans
    
    Use the latest 6th Generation Linode plans (announced in May 2018). The 5th
    Generation plans are publicly deprecated.
    
    See: https://www.linode.com/blog/linode/updated-linode-plans-new-larger-linodes/
    
    * chore: add webhook toggle
    
    Webhooks can be enabled or disabled by setting the `ENABLE_WEBHOOKS` environment
    variable during deployment.
    
    * api: linodemachine: add create validation

--- cmd/main.go
+++ cmd/main.go
@@ -149,6 +149,12 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "LinodeObjectStorageBucket")
 		os.Exit(1)
 	}
+	if os.Getenv("ENABLE_WEBHOOKS") != "false" {
+		if err = (&infrastructurev1alpha1.LinodeMachine{}).SetupWebhookWithManager(mgr); err != nil {
+			setupLog.Error(err, "unable to create webhook", "webhook", "LinodeMachine")
+			os.Exit(1)
+		}
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
