4015c0c06368a7c889605c3e02c7734bdba01dc6
Amol Deodhar <adeodhar@akamai.com>
2024-05-17 11:58:05 -0400

    Add new flavors

--- hack/generate-flavors.sh
+++ hack/generate-flavors.sh
@@ -4,17 +4,25 @@ set -euo pipefail
 
 REPO_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
 FLAVORS_DIR="${REPO_ROOT}/templates/flavors"
+SUPPORTED_DISTROS=(
+    "clusterclass-kubeadm"
+    "rke2"
+    "k3s"
+    "kubeadm"
+)
 
-for name in $(find "${FLAVORS_DIR}/"* -maxdepth 0 -type d -print0 | xargs -0 -I {} basename {} | grep -v base | grep -v clusterclass-base ); do
-    # clusterctl expects clusterclass not have the "cluster-template" prefix
-    # except for the actual cluster template using the clusterclass
-    if [[ "$name" == clusterclass* ]]; then
-        kustomize build "${FLAVORS_DIR}/${name}" > "${REPO_ROOT}/templates/${name}.yaml"
-	cp "${FLAVORS_DIR}/${name}/cluster-template.yaml" "${REPO_ROOT}/templates/cluster-template-${name}.yaml"
-    else
-        kustomize build "${FLAVORS_DIR}/${name}" > "${REPO_ROOT}/templates/cluster-template-${name}.yaml"
+for distro in ${SUPPORTED_DISTROS[@]}; do
+    if [[ $distro == "clusterclass-kubeadm" ]]; then
+        # clusterctl expects clusterclass not have the "cluster-template" prefix
+        # except for the actual cluster template using the clusterclass
+        echo "****** Generating clusterclass-kubeadm flavor ******"
+        kustomize build "${FLAVORS_DIR}/${distro}" > "${REPO_ROOT}/templates/${distro}.yaml"
+        cp "${FLAVORS_DIR}/${distro}/cluster-template.yaml" "${REPO_ROOT}/templates/cluster-template-${distro}.yaml"
+        continue
     fi
-done
 
-# move the default template to the default file expected by clusterctl
-mv "${REPO_ROOT}/templates/cluster-template-default.yaml" "${REPO_ROOT}/templates/cluster-template.yaml"
+    for name in $(find "${FLAVORS_DIR}/${distro}/"* -maxdepth 0 -type d -print0 | xargs -0 -I {} basename {}); do
+        echo "****** Generating ${distro}-${name} flavor ******"
+        kustomize build "${FLAVORS_DIR}/${distro}/${name}" > "${REPO_ROOT}/templates/cluster-template-${distro}-${name}.yaml"
+    done
+done
