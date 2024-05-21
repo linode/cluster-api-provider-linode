aba757a83377a6da940dab34ed7505d29ca4707a
Amol Deodhar <adeodhar@akamai.com>
2024-05-21 09:55:53 -0400

    changes to flavor script to separate distros from clusterclass

--- hack/generate-flavors.sh
+++ hack/generate-flavors.sh
@@ -10,17 +10,20 @@ SUPPORTED_DISTROS=(
     "k3s"
     "kubeadm"
 )
+SUPPORTED_CLUSTERCLASSES=(
+    "clusterclass-kubeadm"
+)
+
+for clusterclass in ${SUPPORTED_CLUSTERCLASSES[@]}; do
+    # clusterctl expects clusterclass not have the "cluster-template" prefix
+    # except for the actual cluster template using the clusterclass
+    echo "****** Generating clusterclass-kubeadm flavor ******"
+    kustomize build "${FLAVORS_DIR}/${clusterclass}" > "${REPO_ROOT}/templates/${clusterclass}.yaml"
+    cp "${FLAVORS_DIR}/${clusterclass}/cluster-template.yaml" "${REPO_ROOT}/templates/cluster-template-${clusterclass}.yaml"
+done
+
 
 for distro in ${SUPPORTED_DISTROS[@]}; do
-    if [[ $distro == "clusterclass-kubeadm" ]]; then
-        # clusterctl expects clusterclass not have the "cluster-template" prefix
-        # except for the actual cluster template using the clusterclass
-        echo "****** Generating clusterclass-kubeadm flavor ******"
-        kustomize build "${FLAVORS_DIR}/${distro}" > "${REPO_ROOT}/templates/${distro}.yaml"
-        cp "${FLAVORS_DIR}/${distro}/cluster-template.yaml" "${REPO_ROOT}/templates/cluster-template-${distro}.yaml"
-        continue
-    fi
-
     for name in $(find "${FLAVORS_DIR}/${distro}/"* -maxdepth 0 -type d -print0 | xargs -0 -I {} basename {}); do
         echo "****** Generating ${distro}-${name} flavor ******"
         kustomize build "${FLAVORS_DIR}/${distro}/${name}" > "${REPO_ROOT}/templates/cluster-template-${distro}-${name}.yaml"
