65df73382e86f28de6bde49d389906b9809a8e1d
Mateusz Urbanek <murbanek@akamai.com>
2024-06-17 17:37:32 +0200

    test: add smoke tests to few parts of code
    Signed-off-by: Mateusz Urbanek <murbanek@akamai.com>

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2023 Akamai Technologies, Inc.
+Copyright 2023-2024 Akamai Technologies, Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
---
+++ observability/tracing/tracing_test.go
@@ -0,0 +1,58 @@
+/*
+Copyright 2024 Akamai Technologies, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package tracing
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/otel/sdk/resource"
+)
+
+func TestSetup(t *testing.T) {
+	t.Parallel()
+
+	for name, tc := range map[string]struct {
+		env      map[string]string
+		resource *resource.Resource
+	}{
+		"smoke": {
+			env:      make(map[string]string),
+			resource: resource.Default(),
+		},
+	} {
+		tc := tc
+
+		t.Run(name, func(t *testing.T) {
+			for k, v := range tc.env {
+				t.Setenv(k, v)
+			}
+
+			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+			defer cancel()
+
+			shutdown, err := Setup(ctx, tc.resource)
+			require.NoError(t, err)
+
+			err = shutdown(ctx)
+			assert.NoError(t, err)
+		})
+	}
+}
