02a91c055de7cc66305f51e1c828c50cccd65157
Amol Deodhar <adeodhar@akamai.com>
2024-07-05 09:05:24 -0400

    fixup! [feat]: add cluster template, use CAAPH to install cilium for the CNI (#96)

--- controller/linodemachine_controller_helpers.go
+++ controller/linodemachine_controller_helpers.go
@@ -53,7 +53,7 @@ const maxBootstrapDataBytes = 16384
 var (
 	errNoPublicIPv4Addrs      = errors.New("no public ipv4 addresses set")
 	errNoPublicIPv6Addrs      = errors.New("No public IPv6 address set")
-	errNoPublicIPv6SLAACAddrs = errors.New("No public SLAAC IPv6 address set")
+	errNoPublicIPv6SLAACAddrs = errors.New("No public SLAAC address set")
 )
 
 func (r *LinodeMachineReconciler) newCreateConfig(ctx context.Context, machineScope *scope.MachineScope, tags []string, logger logr.Logger) (*linodego.InstanceCreateOptions, error) {
