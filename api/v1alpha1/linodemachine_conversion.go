06a4d4980294ee55bb627f63119b56f579609f27
Evan Johnson <ejohnson@akamai.com>
2024-07-26 11:30:39 -0400

    migrate manual conversions to conversion-gen

--- api/v1alpha1/linodemachine_conversion.go
+++ api/v1alpha1/linodemachine_conversion.go
@@ -19,6 +19,7 @@ package v1alpha1
 import (
 	"errors"
 
+	utilconversion "sigs.k8s.io/cluster-api/util/conversion"
 	"sigs.k8s.io/controller-runtime/pkg/conversion"
 
 	infrastructurev1alpha2 "github.com/linode/cluster-api-provider-linode/api/v1alpha2"
@@ -31,54 +32,15 @@ func (src *LinodeMachine) ConvertTo(dstRaw conversion.Hub) error {
 		return errors.New("failed to convert LinodeMachine version from v1alpha1 to v1alpha2")
 	}
 
-	// ObjectMeta
-	dst.ObjectMeta = src.ObjectMeta
-
-	// Spec
-	dst.Spec.InstanceID = src.Spec.InstanceID
-	dst.Spec.Region = src.Spec.Region
-	dst.Spec.Type = src.Spec.Type
-	dst.Spec.Group = src.Spec.Group
-	dst.Spec.RootPass = src.Spec.RootPass
-	dst.Spec.AuthorizedKeys = src.Spec.AuthorizedKeys
-	dst.Spec.AuthorizedUsers = src.Spec.AuthorizedUsers
-	dst.Spec.BackupID = src.Spec.BackupID
-	dst.Spec.Image = src.Spec.Image
-	if src.Spec.Interfaces != nil {
-		dst.Spec.Interfaces = []infrastructurev1alpha2.InstanceConfigInterfaceCreateOptions{}
-		for _, configInterface := range src.Spec.Interfaces {
-			dst.Spec.Interfaces = append(dst.Spec.Interfaces, infrastructurev1alpha2.InstanceConfigInterfaceCreateOptions{
-				IPAMAddress: configInterface.IPAMAddress,
-				Label:       configInterface.Label,
-				Purpose:     configInterface.Purpose,
-				Primary:     configInterface.Primary,
-				SubnetID:    configInterface.SubnetID,
-				IPv4:        (*infrastructurev1alpha2.VPCIPv4)(configInterface.IPv4),
-				IPRanges:    configInterface.IPRanges,
-			})
-		}
+	if err := Convert_v1alpha1_LinodeMachine_To_v1alpha2_LinodeMachine(src, dst, nil); err != nil {
+		return err
 	}
-	dst.Spec.BackupsEnabled = src.Spec.BackupsEnabled
-	dst.Spec.PrivateIP = src.Spec.PrivateIP
-	dst.Spec.ProviderID = src.Spec.ProviderID
-	dst.Spec.Tags = src.Spec.Tags
-	dst.Spec.FirewallID = src.Spec.FirewallID
-	dst.Spec.OSDisk = (*infrastructurev1alpha2.InstanceDisk)(src.Spec.OSDisk)
-	if src.Spec.DataDisks != nil {
-		dst.Spec.DataDisks = map[string]*infrastructurev1alpha2.InstanceDisk{}
-		for diskName, dataDisk := range src.Spec.DataDisks {
-			dst.Spec.DataDisks[diskName] = (*infrastructurev1alpha2.InstanceDisk)(dataDisk)
-		}
-	}
-	dst.Spec.CredentialsRef = src.Spec.CredentialsRef
 
-	// Status
-	dst.Status.Ready = src.Status.Ready
-	dst.Status.Conditions = src.Status.Conditions
-	dst.Status.FailureMessage = src.Status.FailureMessage
-	dst.Status.FailureReason = src.Status.FailureReason
-	dst.Status.Addresses = src.Status.Addresses
-	dst.Status.InstanceState = src.Status.InstanceState
+	// Manually restore data from annotations
+	restored := &LinodeMachine{}
+	if ok, err := utilconversion.UnmarshalData(src, restored); err != nil || !ok {
+		return err
+	}
 
 	return nil
 }
@@ -90,53 +52,32 @@ func (dst *LinodeMachine) ConvertFrom(srcRaw conversion.Hub) error {
 		return errors.New("failed to convert LinodeMachine version from v1alpha2 to v1alpha1")
 	}
 
-	// ObjectMeta
-	dst.ObjectMeta = src.ObjectMeta
-
-	// Spec
-	dst.Spec.InstanceID = src.Spec.InstanceID
-	dst.Spec.Region = src.Spec.Region
-	dst.Spec.Type = src.Spec.Type
-	dst.Spec.Group = src.Spec.Group
-	dst.Spec.RootPass = src.Spec.RootPass
-	dst.Spec.AuthorizedKeys = src.Spec.AuthorizedKeys
-	dst.Spec.AuthorizedUsers = src.Spec.AuthorizedUsers
-	dst.Spec.BackupID = src.Spec.BackupID
-	dst.Spec.Image = src.Spec.Image
-	if src.Spec.Interfaces != nil {
-		dst.Spec.Interfaces = []InstanceConfigInterfaceCreateOptions{}
-		for _, configInterface := range src.Spec.Interfaces {
-			dst.Spec.Interfaces = append(dst.Spec.Interfaces, InstanceConfigInterfaceCreateOptions{
-				IPAMAddress: configInterface.IPAMAddress,
-				Label:       configInterface.Label,
-				Purpose:     configInterface.Purpose,
-				Primary:     configInterface.Primary,
-				SubnetID:    configInterface.SubnetID,
-				IPv4:        (*VPCIPv4)(configInterface.IPv4),
-				IPRanges:    configInterface.IPRanges,
-			})
-		}
+	if err := Convert_v1alpha2_LinodeMachine_To_v1alpha1_LinodeMachine(src, dst, nil); err != nil {
+		return err
 	}
-	dst.Spec.BackupsEnabled = src.Spec.BackupsEnabled
-	dst.Spec.ProviderID = src.Spec.ProviderID
-	dst.Spec.PrivateIP = src.Spec.PrivateIP
-	dst.Spec.Tags = src.Spec.Tags
-	dst.Spec.FirewallID = src.Spec.FirewallID
-	dst.Spec.OSDisk = (*InstanceDisk)(src.Spec.OSDisk)
-	if src.Spec.DataDisks != nil {
-		dst.Spec.DataDisks = map[string]*InstanceDisk{}
-		for diskName, dataDisk := range src.Spec.DataDisks {
-			dst.Spec.DataDisks[diskName] = (*InstanceDisk)(dataDisk)
-		}
-	}
-	dst.Spec.CredentialsRef = src.Spec.CredentialsRef
 
-	// Status
-	dst.Status.Ready = src.Status.Ready
-	dst.Status.Conditions = src.Status.Conditions
-	dst.Status.FailureMessage = src.Status.FailureMessage
-	dst.Status.FailureReason = src.Status.FailureReason
-	dst.Status.Addresses = src.Status.Addresses
-	dst.Status.InstanceState = src.Status.InstanceState
+	// Preserve Hub data on down-conversion.
+	if err := utilconversion.MarshalData(src, dst); err != nil {
+		return err
+	}
+
 	return nil
+}
+
+// ConvertTo converts this LinodeMachineList to the Hub version (v1alpha2).
+func (src *LinodeMachineList) ConvertTo(dstRaw conversion.Hub) error {
+	dst, ok := dstRaw.(*infrastructurev1alpha2.LinodeMachineList)
+	if !ok {
+		return errors.New("failed to convert LinodeMachine version from v1alpha1 to v1alpha2")
+	}
+	return Convert_v1alpha1_LinodeMachineList_To_v1alpha2_LinodeMachineList(src, dst, nil)
+}
+
+// ConvertFrom converts from the Hub version (v1alpha2) to this version.
+func (dst *LinodeMachineList) ConvertFrom(srcRaw conversion.Hub) error {
+	src, ok := srcRaw.(*infrastructurev1alpha2.LinodeMachineList)
+	if !ok {
+		return errors.New("failed to convert LinodeMachine version from v1alpha2 to v1alpha1")
+	}
+	return Convert_v1alpha2_LinodeMachineList_To_v1alpha1_LinodeMachineList(src, dst, nil)
 }
