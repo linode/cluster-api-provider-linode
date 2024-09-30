9da5d152faf90cca55d1354bcd875073da725b42
unnatiagg <aggarwal.un@northeastern.edu>
2024-09-30 15:21:01 -0400

    adding testcases for validate cluster

--- api/v1alpha2/linodecluster_webhook_test.go
+++ api/v1alpha2/linodecluster_webhook_test.go
@@ -24,8 +24,11 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/mock/gomock"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/utils/ptr"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/linode/cluster-api-provider-linode/mock"
 
@@ -85,6 +88,10 @@ func TestValidateLinodeCluster(t *testing.T) {
 
 func TestValidateCreate(t *testing.T) {
 	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockK8sClient := mock.NewMockK8sClient(ctrl)
 
 	var (
 		cluster = LinodeCluster{
@@ -99,6 +106,33 @@ func TestValidateCreate(t *testing.T) {
 				},
 			},
 		}
+		validCluster = LinodeCluster{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "example",
+				Namespace: "example",
+			},
+			Spec: LinodeClusterSpec{
+				Region: "us-sea",
+				Network: NetworkSpec{
+					LoadBalancerType: "NodeBalancer",
+				},
+			},
+		}
+		credentialsRefCluster = LinodeCluster{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "example",
+				Namespace: "example",
+			},
+			Spec: LinodeClusterSpec{
+				CredentialsRef: &corev1.SecretReference{
+					Name: "rke2-cluster-credentials",
+				},
+				Region: "us-sea",
+				Network: NetworkSpec{
+					LoadBalancerType: "NodeBalancer",
+				},
+			},
+		}
 		validator = &linodeClusterValidator{}
 	)
 
@@ -106,11 +140,62 @@ func TestValidateCreate(t *testing.T) {
 		OneOf(
 			Path(
 				Call("invalid region", func(ctx context.Context, mck Mock) {
-					mck.LinodeClient.EXPECT().GetRegion(gomock.Any(), gomock.Any()).Return(nil, errors.New("invalid region")).AnyTimes()
+
 				}),
 				Result("error", func(ctx context.Context, mck Mock) {
 					_, err := validator.ValidateCreate(ctx, &cluster)
 					assert.Error(t, err)
+				}),
+			),
+		),
+		OneOf(
+			Path(
+				Call("valid region", func(ctx context.Context, mck Mock) {
+
+				}),
+				Result("no error", func(ctx context.Context, mck Mock) {
+					_, err := validator.ValidateCreate(ctx, &validCluster)
+					assert.NoError(t, err)
+				}),
+			),
+		),
+		OneOf(
+			Path(
+				Call("error fetching the token", func(ctx context.Context, mck Mock) {
+					mockK8sClient.EXPECT().Get(gomock.Any(), gomock.Any(), gomock.Any()).Return(errors.New("test error"))
+
+				}),
+				Result("error", func(ctx context.Context, mck Mock) {
+					_, err := getCredentialDataFromRef(ctx, mockK8sClient, *credentialsRefCluster.Spec.CredentialsRef, cluster.GetNamespace())
+					assert.Error(t, err)
+
+				}),
+			),
+		),
+		OneOf(
+			Path(
+				Call("verfied linodeClient", func(ctx context.Context, mck Mock) {
+					mockK8sClient.EXPECT().Get(ctx, gomock.Any(), gomock.Any()).
+						DoAndReturn(func(ctx context.Context, key types.NamespacedName, obj *corev1.Secret, opts ...client.GetOption) error {
+							cred := corev1.Secret{
+								ObjectMeta: metav1.ObjectMeta{
+									Name:      "rke2-cluster-credentials",
+									Namespace: "example",
+								},
+								Data: map[string][]byte{
+									"apiToken": []byte("token"),
+								},
+							}
+							*obj = cred
+
+							return nil
+						}).AnyTimes()
+
+				}),
+				Result("valid", func(ctx context.Context, mck Mock) {
+					str, err := getCredentialDataFromRef(ctx, mockK8sClient, *credentialsRefCluster.Spec.CredentialsRef, cluster.GetNamespace())
+					assert.NoError(t, err)
+					assert.Equal(t, []byte("token"), str)
 				}),
 			),
 		),
