5983145f577f03d7b09f5fdaec2ed94f185213aa
Cindy Bang <cbang@akamai.com>
2024-12-03 08:47:36 -0500

    clients: add AWS SDK clients

--- mock/common.go
+++ mock/common.go
@@ -13,12 +13,16 @@ type MockClient interface {
 func (MockLinodeClient) mock()      {}
 func (MockK8sClient) mock()         {}
 func (MockAkamEdgeDNSClient) mock() {}
+func (MockS3Client) mock()          {}
+func (MockS3PresignClient) mock()   {}
 
 // MockClients holds mock clients that may be instantiated.
 type MockClients struct {
 	LinodeClient      *MockLinodeClient
 	K8sClient         *MockK8sClient
 	AkamEdgeDNSClient *MockAkamEdgeDNSClient
+	S3Client          *MockS3Client
+	S3PresignClient   *MockS3PresignClient
 }
 
 func (mc *MockClients) Build(client MockClient, ctrl *gomock.Controller) {
@@ -29,5 +33,9 @@ func (mc *MockClients) Build(client MockClient, ctrl *gomock.Controller) {
 		mc.K8sClient = NewMockK8sClient(ctrl)
 	case MockAkamEdgeDNSClient, *MockAkamEdgeDNSClient:
 		mc.AkamEdgeDNSClient = NewMockAkamEdgeDNSClient(ctrl)
+	case MockS3Client, *MockS3Client:
+		mc.S3Client = NewMockS3Client(ctrl)
+	case MockS3PresignClient, *MockS3PresignClient:
+		mc.S3PresignClient = NewMockS3PresignClient(ctrl)
 	}
 }
