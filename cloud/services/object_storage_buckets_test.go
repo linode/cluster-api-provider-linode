fa1fae2cdcfb8a33e88021f96389b91c21f756dc
Ashley Dumaine <ashley.dumaine@gmail.com>
2025-06-27 12:50:01 -0400

    more coverage with delete bucket test

--- cloud/services/object_storage_buckets_test.go
+++ cloud/services/object_storage_buckets_test.go
@@ -3,17 +3,17 @@ package services
 import (
 	"context"
 	"fmt"
-	v1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/apimachinery/pkg/types"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 	"testing"
 
 	"github.com/linode/linodego"
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/mock/gomock"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	infrav1alpha2 "github.com/linode/cluster-api-provider-linode/api/v1alpha2"
 	"github.com/linode/cluster-api-provider-linode/cloud/scope"
@@ -307,6 +307,92 @@ func TestCreateS3ClientWithAccessKey(t *testing.T) {
 				assert.ErrorContains(t, err, testcase.expectedError.Error())
 			} else {
 				assert.NotNil(t, s3Client)
+			}
+		})
+	}
+}
+
+func TestDeleteBucket(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name          string
+		bScope        *scope.ObjectStorageBucketScope
+		expectedError error
+		expects       func(k8s *mock.MockK8sClient, lc *mock.MockLinodeClient)
+	}{
+		{
+			name: "Error - failed to purge all objects",
+			bScope: &scope.ObjectStorageBucketScope{
+				Bucket: &infrav1alpha2.LinodeObjectStorageBucket{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "test-bucket",
+					},
+					Spec: infrav1alpha2.LinodeObjectStorageBucketSpec{
+						Region: "test-region",
+						AccessKeyRef: &v1.ObjectReference{
+							Name: "test-bucket",
+						},
+					},
+				},
+			},
+			expects: func(k8s *mock.MockK8sClient, lc *mock.MockLinodeClient) {
+				k8s.EXPECT().Get(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).DoAndReturn(func(ctx context.Context, name types.NamespacedName, obj *v1.Secret, opts ...client.GetOption) error {
+					secret := v1.Secret{
+						ObjectMeta: metav1.ObjectMeta{
+							Name: "test-bucket-obj-key",
+						},
+						Data: map[string][]byte{
+							"access": []byte("test-access-key"),
+							"secret": []byte("test-secret-key"),
+							"bucket": []byte("test-bucket"),
+						},
+					}
+					*obj = secret
+					return nil
+				})
+			},
+			expectedError: fmt.Errorf("failed to purge all objects"),
+		},
+		{
+			name: "Error - failed to create S3 client",
+			bScope: &scope.ObjectStorageBucketScope{
+				Bucket: &infrav1alpha2.LinodeObjectStorageBucket{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "test-bucket",
+					},
+					Spec: infrav1alpha2.LinodeObjectStorageBucketSpec{
+						Region: "test-region",
+						AccessKeyRef: &v1.ObjectReference{
+							Name: "test",
+						},
+					},
+				},
+			},
+			expects: func(k8s *mock.MockK8sClient, lc *mock.MockLinodeClient) {
+				k8s.EXPECT().Get(gomock.Any(), gomock.Any(), gomock.Any()).Return(errors.NewNotFound(schema.GroupResource{}, ""))
+			},
+			expectedError: fmt.Errorf("failed to create S3 client"),
+		},
+	}
+	for _, tt := range tests {
+		testcase := tt
+		t.Run(testcase.name, func(t *testing.T) {
+			t.Parallel()
+
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			mockK8s := mock.NewMockK8sClient(ctrl)
+			testcase.bScope.Client = mockK8s
+			mockClient := mock.NewMockLinodeClient(ctrl)
+			testcase.bScope.LinodeClient = mockClient
+
+			testcase.expects(mockK8s, mockClient)
+
+			err := DeleteBucket(t.Context(), testcase.bScope)
+			if testcase.expectedError != nil {
+				assert.ErrorContains(t, err, testcase.expectedError.Error())
 			}
 		})
 	}
